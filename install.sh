2c79d9ab605442ac6c11089d2d79d4d73c5dc1c6
Nguyen San <vinhsannguyen91@gmail.com>
2017-09-04 15:04:40 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!bash
+
+curl -O https://raw.githubusercontent.com/sandangel/angular-cli-docker/master/Makefile
+curl -O https://raw.githubusercontent.com/sandangel/angular-cli-docker/master/docker-compose.yml
